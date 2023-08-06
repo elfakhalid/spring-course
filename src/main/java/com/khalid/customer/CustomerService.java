b41a0bc97e27110d4de0766fe221b5017aa7f33e
elfakhalid <elfatouaki48@gmail.com>
2023-08-06 19:02:44 +0100

    added exception handler

--- src/main/java/com/khalid/customer/CustomerService.java
+++ src/main/java/com/khalid/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.khalid.customer;
 
+import com.khalid.exceptions.ResourceNotFound;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +19,6 @@ public class CustomerService {
 
     public Customer getCustomer(Integer id){
         return customerDAO.selectCustomer(id).
-                orElseThrow(()->new IllegalArgumentException("customer with id [%s] not found".formatted(id)));
+                orElseThrow(()->new ResourceNotFound("customer with id [%s] not found".formatted(id)));
     }
 }
